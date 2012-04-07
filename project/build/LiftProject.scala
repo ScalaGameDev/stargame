790c750831d88bf97ce7b505138dc0f71f80c9af
Tommy C. Li <tommycli@ucla.edu>
2012-04-07 11:24:27 -0600

    Add Logger back

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -22,14 +22,15 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info)
     "net.liftweb" %% "lift-openid" % liftVersion % "compile->default",
     "net.liftweb" %% "lift-json" % liftVersion % "compile->default",
     "com.typesafe.akka" % "akka-actor" % "2.0",
-    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default"
+    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
+    "ch.qos.logback" % "logback-classic" % "1.0.1"
   ) ++ super.libraryDependencies
   
-  override def ivyXML =
+  /*override def ivyXML =
     <dependencies>
       <dependency org="org.slf4j" name="slf4j-api" rev="1.5.11" force="true">
       </dependency>
       <exclude module="logback-classic" />
       <exclude module="logback-core" />
-    </dependencies>
+    </dependencies>*/
 }
